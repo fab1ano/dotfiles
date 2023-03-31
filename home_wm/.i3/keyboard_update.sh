dd3c84f08afa62ac615a22ab4eb95b804c74c276
Fabian Fleischer <fab.fleischer@outlook.com>
2023-03-31 14:00:55 -0400

    Set input props for touchpad

--- home_wm/.i3/keyboard_update.sh
+++ home_wm/.i3/keyboard_update.sh
@@ -5,3 +5,9 @@ if [ "$1 $3" == "XIDeviceEnabled XISlaveKeyboard"  ]
 then
 	setxkbmap -option altwin:swap_lalt_lwin
 fi
+
+if [ "$1 $3 $4" == "XIDeviceEnabled XISlavePointer HTX USB HID Device HTX HID Device Touchpad"  ]
+then
+	xinput --set-prop $2 'libinput Tapping Enabled' 1
+	xinput --set-prop $2 'libinput Accel Speed' 0.5
+fi
