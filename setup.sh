7a73f95ba37311cf3f57f9b72d1482cad6bafa08
Fabian Fleischer <fab.fleischer@outlook.com>
2023-09-22 10:09:25 -0400

    Update i3wm keyring

--- setup.sh
+++ setup.sh
@@ -76,8 +76,8 @@ sudo chsh -s /usr/bin/zsh $USERNAME
 # Window Manager
 if [[ -n "$install_wm" ]]; then
   # Install i3wm
-  /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2022.02.17_all.deb keyring.deb SHA256:52053550c4ecb4e97c48900c61b2df4ec50728249d054190e8a0925addb12fc6
-  sudo dpkg -i ./keyring.deb
+  /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
+  sudo apt install ./keyring.deb
   rm ./keyring.deb
   echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list >/dev/null
   sudo apt update
