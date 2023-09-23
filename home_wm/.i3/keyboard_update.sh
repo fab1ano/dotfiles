b86d57848119571a6beee67103d3e429c0297181
Fabian Fleischer <fab.fleischer@outlook.com>
2023-09-23 12:07:21 -0400

    Switch mouse keys

--- home_wm/.i3/keyboard_update.sh
+++ home_wm/.i3/keyboard_update.sh
@@ -18,7 +18,7 @@ fi
 # Ball Mouse
 if [ "$1 $3 $4" == "XIDeviceEnabled XISlavePointer Kensington Expert Wireless TB Mouse"  ]
 then
-	xinput --set-button-map $2 8 3 1 4 5 6 7 2 9
+	xinput --set-button-map $2 8 1 3 4 5 6 7 2 9
 	xinput --set-prop $2 'libinput Accel Speed' 1.0
 	xinput --set-prop $2 'libinput Scrolling Pixel Distance' 50
 fi
