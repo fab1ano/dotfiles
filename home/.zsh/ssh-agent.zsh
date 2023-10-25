faba322bd38a55ee0aee17e16e8f9cd38619ca00
Fabian Fleischer <fab.fleischer@outlook.com>
2023-10-25 15:55:43 -0400

    Update ssh-agent.zsh

--- home/.zsh/ssh-agent.zsh
+++ home/.zsh/ssh-agent.zsh
@@ -2,29 +2,25 @@ typeset ssh_environment
 
 function start_ssh_agent() {
 	local lifetime
-	local -a identities
-
-	zstyle -s :plugins:ssh-agent lifetime lifetime
-
-	ssh-agent -s ${lifetime:+-t} ${lifetime} | sed 's/^echo/#echo/' >! $ssh_environment
-	chmod 600 $ssh_environment
-	source $ssh_environment > /dev/null
-
-	zstyle -a :plugins:ssh-agent identities identities
 
 	echo starting ssh-agent...
-	ssh-add $HOME/.ssh/${^identities}
+
+	ssh-agent -s | sed 's/^echo/#echo/' >! $ssh_environment
+	chmod 600 $ssh_environment
+	source $ssh_environment > /dev/null
 }
 
 ssh_environment="$HOME/.ssh/environment-$HOST"
 
-if [[ -f "$ssh_environment" ]]; then
-	source $ssh_environment > /dev/null
-	ps x | grep ssh-agent | grep -q $SSH_AGENT_PID || {
+if [[ $UID -ne 0 ]]; then
+	if [[ -f "$ssh_environment" ]]; then
+		source $ssh_environment > /dev/null
+		ps x | grep ssh-agent | grep -q $SSH_AGENT_PID || {
+			start_ssh_agent
+		}
+	else
 		start_ssh_agent
-	}
-else
-	start_ssh_agent
+	fi
 fi
 
 unset ssh_environment
